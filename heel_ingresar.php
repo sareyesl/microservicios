e91a74903d07522f38790e35327817205b0b9922
Santiago A Reyes Leon <107640858+sareyesl@users.noreply.github.com>
2022-11-29 12:32:22 -0500

    Update heel_ingresar.php

--- heel_ingresar.php
+++ heel_ingresar.php
@@ -3,7 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     require_once __DIR__ . '/vendor/autoload.php';
     // Crear Cliente---------------------------------------------------------------------
-    $client = new MongoDB\Client('mongodb+srv://admin:123@cluster0.lvbvi.mongodb.net/?retryWrites=true&w=majority');
+    $client = new MongoDB\Client('mongodb+srv://kat:1234@cluster0.sacgk.mongodb.net/?retryWrites=true&w=majority');
     // Traer Base de datos-----------------------------------------------------------------
     $database = $client->MultimediaS;
     // Crear o Traer coleccion-----------------------------------------------------------
